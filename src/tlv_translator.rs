3127b628e84ba636dfdd8be9efbcf5abf8da8c4b
Brendan Mesters @BreViBo <brendanmesters@gmail.com>
2025-01-08 16:16:16 +0100

    Worked on Tlv Headers and Tlv parsing
    I created parsing for tlv headers, worked a bit more on the parsing of
    the actual tlv data, as well as some of the boilerplate around the juicy
    stuff.
    
    I am however running nto a strange issue where ~80% of my tlv frames
    show consistent non-random garbage values.
    
    Often they are in the range of (but not exactly equal to):
    - type: 1071847877
     len: 1055430263

--- src/tlv_translator.rs
+++ src/tlv_translator.rs
@@ -26,7 +26,7 @@ struct FrameHeaderBackend {
     subframe_number: u32,
 }
 
-/// `RawHeader`  is a `union` type, which holds 40 bytes of raw
+/// `FrameHeader`  is a `union` type, which holds 40 bytes of raw
 /// data, and links those to the different headers defined in its
 /// `headers` field.
 #[repr(C)]
@@ -41,8 +41,142 @@ impl FrameHeader {
     }
 
     fn frame_len(&self) -> usize {
+        // Union field access is ALWAYS specified as unsafe
+        // This access is safe, since the types are stored as
+        // C represented arrays, meaning the data is just thrown
+        // on a large heap.
         unsafe { self.headers.package_length }.try_into().unwrap()
     }
+
+    fn frame_num(&self) -> usize {
+        // Union field access is ALWAYS specified as unsafe
+        // This access is safe, since the types are stored as
+        // C represented arrays, meaning the data is just thrown
+        // on a large heap.
+        unsafe { self.headers.frame_number }.try_into().unwrap()
+    }
+
+    fn tlv_count(&self) -> usize {
+        // Union field access is ALWAYS specified as unsafe
+        // This access is safe, since the types are stored as
+        // C represented arrays, meaning the data is just thrown
+        // on a large heap.
+        unsafe { self.headers.num_tlv }.try_into().unwrap()
+    }
+
+    fn obj_count(&self) -> usize {
+        // Union field access is ALWAYS specified as unsafe
+        // This access is safe, since the types are stored as
+        // C represented arrays, meaning the data is just thrown
+        // on a large heap.
+        unsafe { self.headers.num_detected_obj }.try_into().unwrap()
+    }
+}
+
+/// Enum describing the different TLV frame types
+/// as defined in the [specifications of TI](https://dev.ti.com/tirex/explore/content/radar_toolbox_2_30_00_12/software_docs/Understanding_UART_Data_Output_Format.html#statistics)
+#[derive(Clone, Copy)]
+enum TlvType {
+    DetectedPoints = 1,
+    RangeProfile = 2,
+    NoiseFloorProfile = 3,
+    AzimuthStaticHeatmap = 4,
+    RangeDopplerHeatmap = 5,
+    PerformanceStatistics = 6,
+    SideInforForDetectedPoints = 7,
+    AzimuthElevationStaticHeatmap = 8,
+    TemperatureStatistics = 9,
+}
+
+impl TlvType {
+    /// Parses an index number (as specified by TI) into
+    /// the related TlvType, returns None on an invalid
+    /// number
+    ///
+    /// # Arguments
+    /// * `n`: Must be in range (1-9)
+    fn from_num(n: usize) -> Option<TlvType> {
+        if !(1..=9).contains(&n) {
+            return None;
+        }
+        let result = match n {
+            1 => TlvType::DetectedPoints,
+            2 => TlvType::RangeProfile,
+            3 => TlvType::NoiseFloorProfile,
+            4 => TlvType::AzimuthStaticHeatmap,
+            5 => TlvType::RangeDopplerHeatmap,
+            6 => TlvType::PerformanceStatistics,
+            7 => TlvType::SideInforForDetectedPoints,
+            8 => TlvType::AzimuthElevationStaticHeatmap,
+            9 => TlvType::TemperatureStatistics,
+            _ => panic!("This code should be unreachable."),
+        };
+        return Some(result);
+    }
+}
+
+/// `TlvHeader` is a `union` type, which holds 8 bytes of raw
+/// data and links those to the `tlv_type` and `tlv_len` field
+/// in the `TlvheaderBackend` object, stored in te `headers` field
+#[repr(C)]
+union TlvHeader {
+    data: [u8; 8],
+    headers: [u32; 2],
+}
+
+impl TlvHeader {
+    /// Extracts the TLV header from a u8 vector.
+    /// Tis operation is **destructive** and removes
+    /// the 8 bytes parsed if succeful.
+    ///
+    /// # Arguments
+    /// * `input`:  a reference to a byte vector contaiing
+    ///             the data from which a TLV header should
+    ///             be extracted
+    ///
+    /// # Returns
+    /// * `None`:   if it was not possible to extract 8 bytes
+    ///          from the input, the input is unchanged
+    /// * If parsing is succesfull it returns `Some(TlvHeader)`
+    ///         and removed the 8 bytes from which it was extracted
+    ///         from the input.
+    fn extract_tlv_header(input: &mut Vec<u8>) -> Option<TlvHeader> {
+        if input.len() < 8 {
+            return None;
+        }
+        let data = input.drain(0..8).collect::<Vec<u8>>().try_into().unwrap();
+        Some(TlvHeader::new(data))
+    }
+
+    fn new(data: [u8; 8]) -> TlvHeader {
+        TlvHeader { data }
+    }
+
+    fn tlv_type(&self) -> usize {
+        // Union field access is ALWAYS specified as unsafe
+        // This access is safe, since the types are stored as
+        // C represented arrays, meaning the data is just thrown
+        // on a large heap.
+        unsafe { self.headers[0] }.try_into().unwrap()
+    }
+
+    fn tlv_len(&self) -> usize {
+        // Union field access is ALWAYS specified as unsafe
+        // This access is safe, since the types are stored as
+        // C represented arrays, meaning the data is just thrown
+        // on a large heap.
+        unsafe { self.headers[1] }.try_into().unwrap()
+    }
+}
+
+pub fn is_magic(input: &Vec<u8>, index: usize) -> bool {
+    const MAGIC_WORD: [u8; 8] = [0x02, 0x01, 0x04, 0x03, 0x06, 0x05, 0x08, 0x07];
+
+    let input_size = input.len();
+    if input_size < (index + 8) {
+        return false;
+    }
+    &input[index..(index + 8)] == MAGIC_WORD
 }
 
 /// The function takes a `TLV byte array` as input and
@@ -62,8 +196,6 @@ impl FrameHeader {
 /// * `Vec<PointCloud>` - a vector of frames in the pointcloud
 /// * `usize` - The length of the pointcloud consumed.
 pub fn translate_tlv(input: &mut Vec<u8>) -> (Vec<PointCloud>, usize) {
-    const MAGIC_WORD: [u8; 8] = [0x02, 0x01, 0x04, 0x03, 0x06, 0x05, 0x08, 0x07];
-
     let input_size = input.len();
     if input_size < 8 {
         println!("`translate_tlv` was called with an input of size {}, try calling it with an input of *at least* size 8", input_size);
@@ -71,7 +203,7 @@ pub fn translate_tlv(input: &mut Vec<u8>) -> (Vec<PointCloud>, usize) {
     }
     let mut magic_indexes: Vec<usize> = Vec::new();
     for i in 0..(input_size - 8) {
-        if &input[i..(i + 8)] == MAGIC_WORD {
+        if is_magic(input, i) {
             magic_indexes.push(i);
         }
     }
@@ -82,12 +214,31 @@ pub fn translate_tlv(input: &mut Vec<u8>) -> (Vec<PointCloud>, usize) {
     );
 
     let mut consumed_bytes = 0;
+    let mut i = 0;
+    let mut prev_size = input.len();
+    let mut cur_size = 0;
     for mi in magic_indexes {
-        if mi != consumed_bytes {
-            println!("The current magic index does not seem to be at the current start of the data, magic index at {mi} encountered, while only {consumed_bytes} bytes where consumed");
-        }
+        // if mi != consumed_bytes {
+        //     println!("The current magic index does not seem to be at the current start of the data, magic index at {mi} encountered, while only {consumed_bytes} bytes where consumed");
+        // }
+        println!("Start is a magic word");
         if let Some((pc, n)) = read_frame(input) {
             println!("Succesfully parsed a frame of size {}", n);
+            cur_size = input.len();
+            println!(
+                "Amount of bytes actually consumed: {}",
+                prev_size - cur_size
+            );
+            prev_size = cur_size;
+            if is_magic(input, 0) {
+            } else {
+                println!("Start is NOT a magic word");
+            }
+        }
+        println!("");
+        i += 1;
+        if i > 25 {
+            break;
         }
     }
 
@@ -104,17 +255,86 @@ pub fn translate_tlv(input: &mut Vec<u8>) -> (Vec<PointCloud>, usize) {
 /// * A `PointCloud` object for this single frame
 /// * A `usize`, representing the number of bytes from the input consumed
 /// Or `None` if the frame is not complete.
-fn read_frame(input: &mut Vec<u8>) -> Option<(PointCloud, usize)> {
-    let header: FrameHeader = match read_header(input) {
-        Some(h) => h,
-        None => return None,
-    };
-    if header.frame_len() > input.len() {
-        return None;
-    }
+fn read_frame(data: &mut Vec<u8>) -> Option<(PointCloud, usize)> {
+    let header: FrameHeader = read_header(data)?;
 
-    return Some((PointCloud {}, 0));
+    // Check that the frame is complete
+    let frame_len = header.frame_len();
+    if frame_len > data.len() {
+        return None;
+    } else {
+        // We can read the full frame so can drop the header
+        data.drain(0..40);
+    }
+    println!(
+        "Currently handeling frame nuber {}, with {} objects and {} tlv frames",
+        header.frame_num(),
+        header.obj_count(),
+        header.tlv_count()
+    );
+    // remove 40 from the drainage size as we already removed the header
+    let raw_frame: Vec<u8> = data.drain(0..(frame_len - 40)).collect();
+    parse_frame(raw_frame);
+
+    return Some((PointCloud {}, frame_len));
     // Removing a "frame" can be done effectively with `std::vec::Vec::drain()`.
+}
+
+fn parse_frame(mut data: Vec<u8>) {
+    // Remove the frame header
+    data.drain(0..40);
+    loop {
+        if let Some(tlv_header) = TlvHeader::extract_tlv_header(&mut data) {
+            println!(
+                "Tlv data, type: {} - len: {}, type debug: {:#034b}",
+                tlv_header.tlv_type(),
+                tlv_header.tlv_len(),
+                tlv_header.tlv_type(),
+            );
+            if tlv_header.tlv_len() > data.len() {
+                break;
+            }
+
+            let raw_tlv_data: Vec<u8> = data.drain(0..tlv_header.tlv_len()).collect();
+            match TlvType::from_num(tlv_header.tlv_type()) {
+                Some(TlvType::DetectedPoints) => {}
+                Some(TlvType::RangeProfile) => {
+                    let data = parse_raw_range_profile(raw_tlv_data);
+                }
+                Some(TlvType::NoiseFloorProfile) => {}
+                Some(TlvType::AzimuthStaticHeatmap) => {}
+                Some(TlvType::RangeDopplerHeatmap) => {}
+                Some(TlvType::PerformanceStatistics) => {}
+                Some(TlvType::SideInforForDetectedPoints) => {}
+                Some(TlvType::AzimuthElevationStaticHeatmap) => {}
+                Some(TlvType::TemperatureStatistics) => {}
+                None => break,
+            }
+            println!("- Current data remaining: {}", data.len());
+        } else {
+            break;
+        }
+    }
+    println!("leftover data in frame: {}", data.len())
+}
+
+fn parse_raw_range_profile(data: Vec<u8>) -> Vec<f32> {
+    // We need to parse the data (`rangebin` * 16_bit) into a vector
+    // of `rangebin` values, where every value is interpreted according
+    // to 'Q9' encoding (this is a fixed point encoding) and return
+    // a vector (or array) containing the new values.
+    // https://en.wikipedia.org/wiki/Q_(number_format)
+
+    // Change the byte string into a list of u16s
+    let even_indexed = data.iter().step_by(2);
+    let uneven_indexed = data.iter().skip(1).step_by(2);
+    even_indexed
+        .zip(uneven_indexed)
+        .map(|(lower, upper)| (((*lower as u16) << 8) | *upper as u16))
+        // Now to do q9 encoding according to the following formula P[db] = 20 * log10( 2.^(logMagRange/2^9) )
+        // Accoring to this forum post https://e2e.ti.com/support/sensors-group/sensors/f/sensors-forum/806905/linux-iwr1443boost-interpreting-data-log-magnitude-range-and-doppler-heatmap
+        .map(|log_mag_range| 20.0 * f32::log10(2f32.powi(log_mag_range as i32) / 2f32.powi(9)))
+        .collect()
 }
 
 /// Attempts to read a header located at the the top of the input,
